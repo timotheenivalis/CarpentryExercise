387b6e64925ff48fe4780e04b3efa8f0abe15d72
timotheenivalis <timothee.bonnet@ieu.uzh.ch>
2014-11-23 14:42:15 +0100

    Basic creations

---
+++ S4imulations.R
@@ -0,0 +1,9 @@
+#######################################################
+################ Basic initializations ################
+
+################ Random seed
+set.seed(12)
+################ Counter for the IDs
+CID<-as.integer(1) 
+################ Counter for the current year
+YR<-0 
